30461daa1724b653200a1929e1d529a9cf9bb5ab
Nikolay <nikshowsix@gmail.com>
2025-06-25 20:49:28 +0300

    Loggers

--- src/main/java/ru/hogwarts/school/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ru/hogwarts/school/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFound() {
-        return ResponseEntity.notFound().build();
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<String> handleStudentNotFound(StudentNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
